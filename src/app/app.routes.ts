dce13a2dabb348548d56cc33fe7e93542f7fd389
ronahi39 <ronahi39@hotmail.de>
2025-02-19 11:38:09 +0100

    deleted feature own module and routing and use standalone component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RecipeSearchComponent} from "./features/recipe-search/pages/recipe-search/recipe-search.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' }, // Startseite auf /search umleiten
-  { path: 'search', loadChildren: () => import('./features/recipe-search/recipe-search.module').then(m => m.RecipeSearchModule) },
+  { path: 'search',  component: RecipeSearchComponent },
 
 ];
 
