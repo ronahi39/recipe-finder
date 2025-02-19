207c5fe175d4eeb24a4b31c87f5c9805d2602143
ronahi39 <ronahi39@hotmail.de>
2025-02-19 11:17:21 +0100

    basic routing for recipe-search component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' }, // Startseite auf /search umleiten
+  { path: 'search', loadChildren: () => import('./features/recipe-search/recipe-search.module').then(m => m.RecipeSearchModule) },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/features/recipe-search/recipe-search-routing.module.ts
+++ src/app/features/recipe-search/recipe-search-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeSearchComponent} from "./pages/recipe-search/recipe-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecipeSearchComponent } // Basis-Route für "search"
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
