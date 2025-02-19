88dc22b7c63bd5cd30f36eec28b126b821dfefa4
ronahi39 <ronahi39@hotmail.de>
2025-02-19 11:08:34 +0100

    added feature module structure and added recipe search component

---
+++ src/app/features/recipe-search/recipe-search.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipeSearchRoutingModule } from './recipe-search-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecipeSearchRoutingModule
+  ]
+})
+export class RecipeSearchModule { }
