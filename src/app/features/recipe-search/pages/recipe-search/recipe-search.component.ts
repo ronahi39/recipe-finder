3200ee93781a564b06906614eafc83d57ccaadd5
ronahi39 <ronahi39@hotmail.de>
2025-02-19 13:04:50 +0100

    favicon changes, added search input field

--- src/app/features/recipe-search/pages/recipe-search/recipe-search.component.ts
+++ src/app/features/recipe-search/pages/recipe-search/recipe-search.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-recipe-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './recipe-search.component.html',
   styleUrl: './recipe-search.component.scss'
 })
 export class RecipeSearchComponent {
+
+  searchQuery: string = ''; // Suchbegriff
+
+  searchRecipes() {
+    console.log('Suche nach:', this.searchQuery);
+    // TODO: API-Aufruf oder Filter-Logik hinzufügen
+  }
 
 }
